b2374e29e093014735ed883c8cb9cf872dfd2a39
jiwon <kjwn0241@naver.com>
2023-06-13 15:12:35 +0900

    230613 #3. TodoList scss 작성

---
+++ src/component/TodoTemplate.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import TodoInput from './TodoInput';
+import TodoMain from './TodoMain';
+import TodoHeader from './TodoHeader';
+import TodoItem from './TodoItem';
+
+import './scss/TodoTemplate.scss'
+
+const TodoTemplate = () => {
+  return (
+    <div className='TodoTemplate'>
+      <TodoHeader />
+      <TodoMain />
+      <TodoInput />
+
+    </div>
+  );
+
+}
+
+export default TodoTemplate
